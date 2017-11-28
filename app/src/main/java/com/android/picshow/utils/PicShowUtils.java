ed02f46e79b3d47139164fd91b4cd671fbfbda32
YuntaoWei <weiyuntao_tanyu@163.com>
2017-11-28 11:56:26 +0800

    Add Permission request code.

--- app/src/main/java/com/android/picshow/utils/PicShowUtils.java
+++ app/src/main/java/com/android/picshow/utils/PicShowUtils.java
@@ -1,5 +1,12 @@
 package com.android.picshow.utils;
 
+import android.Manifest;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+
 /**
  * Created by yuntao.wei on 2017/11/28.
  * github:https://github.com/YuntaoWei
@@ -7,4 +14,31 @@ package com.android.picshow.utils;
  */
 
 public class PicShowUtils {
+
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+    //PackageManager.PERMISSION_GRANTED
+    private static final int PERMISSION_GRANTED = 0;
+    //PackageManager.PERMISSION_DENIED
+    private static final int PERMISSION_DENIED = -1;
+
+    public static boolean checkPermissions(Context ctx) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+
+        ArrayList<String> needRequest = new ArrayList<>();
+        for (String permission:PERMISSIONS
+             ) {
+            if(ContextCompat.checkSelfPermission(ctx,permission) == PERMISSION_DENIED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
