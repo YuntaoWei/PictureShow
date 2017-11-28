381fcf7dbf537fd520dbe0710af03fb7874c1dbd
YuntaoWei <weiyuntao_tanyu@163.com>
2017-11-28 11:09:27 +0800

    base code

---
+++ app/src/main/java/com/android/picshow/app/PicShowActivity.java
@@ -0,0 +1,25 @@
+package com.android.picshow.app;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import picshow.android.com.R;
+
+public class PicShowActivity extends AppCompatActivity {
+
+    private ViewPager vPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.picshow_main);
+    }
+
+    private void initView(){}
+
+
+
+
+
+}
---
+++ app/src/main/java/com/android/picshow/app/TimeLinePage.java
@@ -0,0 +1,49 @@
+package com.android.picshow.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yuntao.wei on 2017/11/28.
+ * github:https://github.com/YuntaoWei
+ * blog:http://blog.csdn.net/qq_17541215
+ */
+
+public class TimeLinePage extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
