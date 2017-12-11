f2ee2736343c4c37944798908a2a28d52029c86f
YuntaoWei <weiyuntao_tanyu@163.com>
2017-12-11 11:48:42 +0800

    Add PhotoView and PhotoPage UI controller.

---
+++ app/src/main/java/com/android/picshow/adapter/PhotoPageAdapter.java
@@ -0,0 +1,10 @@
+package com.android.picshow.adapter;
+
+/**
+ * Created by yuntao.wei on 2017/12/11.
+ * github:https://github.com/YuntaoWei
+ * blog:http://blog.csdn.net/qq_17541215
+ */
+
+public class PhotoPageAdapter extends BaseCursorPagerAdapter {
+}
---
+++ app/src/main/java/com/android/picshow/app/PhotoViewFragment.java
@@ -0,0 +1,12 @@
+package com.android.picshow.app;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yuntao.wei on 2017/12/11.
+ * github:https://github.com/YuntaoWei
+ * blog:http://blog.csdn.net/qq_17541215
+ */
+
+public class PhotoViewFragment extends Fragment {
+}
