29e4a1e7875b3791300e7af27aad3dd9842dae40
YuntaoWei <weiyuntao_tanyu@163.com>
2018-06-14 17:42:58 +0800

    Add some edit feature.

---
+++ app/src/main/java/com/android/picshow/app/FilterTestActivity.java
@@ -0,0 +1,10 @@
+package com.android.picshow.app;
+
+/**
+ * Created by yuntao.wei on 2018/5/24.
+ * github:https://github.com/YuntaoWei
+ * blog:http://blog.csdn.net/qq_17541215
+ */
+
+public class FilterTestActivity {
+}
---
+++ app/src/main/java/com/android/picshow/edit/editor/filters/NewFilters.java
@@ -0,0 +1,10 @@
+package com.android.picshow.edit.editor.filters;
+
+/**
+ * Created by yuntao.wei on 2018/5/23.
+ * github:https://github.com/YuntaoWei
+ * blog:http://blog.csdn.net/qq_17541215
+ */
+
+public class NewFilters {
+}
