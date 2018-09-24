c2507b80bdd60dfdb95d1df3455260a701e3035b
Yuntao.Wei <weiyuntao_tanyu@163.com>
2018-09-24 11:50:50 +0800

    Add dialog.

---
+++ app/src/main/java/com/android/picshow/ui/PopupDialog.java
@@ -0,0 +1,16 @@
+package com.android.picshow.ui;
+
+import android.support.v4.widget.PopupWindowCompat;
+import android.widget.PopupWindow;
+
+/**
+ * Created by wyt on 2017/12/30.
+ */
+
+public class PopupDialog extends PopupWindow {
+
+
+
+
+
+}
